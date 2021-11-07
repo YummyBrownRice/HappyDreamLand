093da78d497ac1c5d814ba7ea9219fd3790287d5
BrownRiceStudio <christopher.hyunmin.lee@gmail.com>
2021-11-07 20:54:54 +0900

    Added Scrolling Menu

--- MUSICAL PUZZLE/Assets/Scripts/LevelManager.cs
+++ MUSICAL PUZZLE/Assets/Scripts/LevelManager.cs
@@ -16,11 +16,17 @@ public class LevelManager : MonoBehaviour
         Level level = new Level();
         level.endGoal = new Sequence(levelLoaded.endGoal);
         level.map = new List<Cell>();
+        level.limitations = new List<Limitation>();
 
         foreach (var cell in levelLoaded.map)
         {
             Vector3 coord = new Vector3(cell.coordinate[0], cell.coordinate[1], cell.coordinate[2]);
             level.map.Add(new Cell(coord, (NodeManager.nodeType) cell.type, cell.rotation, new Sequence(cell.sourceSound)));
+        }
+
+        foreach (var limitation in levelLoaded.limitations)
+        {
+            level.limitations.Add(new Limitation((NodeManager.nodeType)limitation.type, limitation.count));
         }
 
         return level;
@@ -32,6 +38,7 @@ public class LevelManager : MonoBehaviour
     {
         public int[] endGoal;
         public LevelCell[] map;
+        public LimitationLoaded[] limitations;
     }
 
     
@@ -39,6 +46,7 @@ public class LevelManager : MonoBehaviour
     {
         public Sequence endGoal;
         public List<Cell> map;
+        public List<Limitation> limitations;
     }
 
     [System.Serializable]
@@ -48,6 +56,26 @@ public class LevelManager : MonoBehaviour
         public int type;
         public int rotation;
         public int[] sourceSound;
+    }
+
+    [System.Serializable]
+    public class LimitationLoaded
+    {
+        public int type;
+        public int count;
+    }
+    
+    public class Limitation
+    {
+        public NodeManager.nodeType type;
+        public int count;
+
+        public Limitation(NodeManager.nodeType type_, int count_)
+        {
+            type = type_;
+            count = count_;
+        }
+
     }
 
     public class Cell
---
+++ MUSICAL PUZZLE/Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private LevelManager levelManager;
+    private NodeManager nodeManager;
+    public List<LevelManager.Limitation> limits;
+
+    public Transform content;
+    public float spacing;
+    public float margin;
+
+    public GameObject tileOBJ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = GameObject.Find("Nodes").GetComponent<LevelManager>();
+        nodeManager = GameObject.Find("Nodes").GetComponent<NodeManager>();
+        LevelManager.Level level = levelManager.Load();
+        limits = level.limitations;
+        int n = limits.Count;
+        //Debug.Log(n);
+        RectTransform r = content.GetComponent<RectTransform>();
+        r.sizeDelta = new Vector2(r.rect.width, 2 * margin + (n - 1) * (spacing));
+        //Debug.Log(r.rect.y);
+        Vector2 Pos = new Vector2(0, -margin);
+        foreach (var limit in limits)
+        {
+            
+            GameObject go = Instantiate(tileOBJ, new Vector2(0, 0), Quaternion.identity, content);
+            go.transform.localPosition = Pos;
+            Pos = Pos - new Vector2(0, spacing);
+            //go.transform.name = Pos.y;
+        }
+    }
+
+}
