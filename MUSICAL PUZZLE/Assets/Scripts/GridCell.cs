54bee13d6e2ca481f58b0cc99848a02ad24a4fd3
BrownRiceStudio <christopher.hyunmin.lee@gmail.com>
2021-11-10 03:39:05 +0900

    Visual Indication

--- MUSICAL PUZZLE/Assets/Scripts/GridCell.cs
+++ MUSICAL PUZZLE/Assets/Scripts/GridCell.cs
@@ -1,10 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class GridCell : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public GameObject highlight;
+    public Transform indicator;
+
+    public Color inputColor;
+    public Color outputColor;
 
     public List<Sprite> GridSprites;
     public Sprite nonSprite;
@@ -37,6 +43,37 @@ public class GridCell : MonoBehaviour
         spriteRenderer.sprite = GridSprites[(int)connectedNode.nodeType];
 
         transform.Rotate(Vector3.forward * -60 * nodeManager.nodes[index].rotation);
+
+        int i = 0;
+        foreach (Transform it in indicator)
+        {
+            //Debug.Log(it.name);
+            int r = connectedNode.rotation;
+            int[] inputDir = (int[])connectedNode.inputDirections.Clone();
+            int[] outputDir = (int[])connectedNode.outputDirections.Clone();
+            SpriteRenderer item = it.GetComponent<SpriteRenderer>();
+
+            for (int a = 0; a < inputDir.Length; a++)
+            {
+                inputDir[a] = (inputDir[a] - r + 6) % 6;
+            }
+            for (int a = 0; a < outputDir.Length; a++)
+            {
+                outputDir[a] = (outputDir[a] - r + 6) % 6;
+            }
+
+            if (inputDir.Contains(i))
+            {
+                //Debug.Log("YO" + i.ToString());
+                item.color = inputColor;
+            }
+            if (outputDir.Contains(i))
+            {
+                //Debug.Log("HEO" + i.ToString());
+                item.color = outputColor;
+            }
+            i += 1;
+        }
     }
 
     public void EmptyCell()
@@ -53,6 +90,7 @@ public class GridCell : MonoBehaviour
         mouseOn = true;
         gridManager.selectedCell = this;
         highlight.SetActive(true);
+        indicator.gameObject.SetActive(true);
 
     }
 
@@ -61,5 +99,6 @@ public class GridCell : MonoBehaviour
         mouseOn = false;
         gridManager.selectedCell = null;
         highlight.SetActive(false);
+        indicator.gameObject.SetActive(false);
     }
 }
